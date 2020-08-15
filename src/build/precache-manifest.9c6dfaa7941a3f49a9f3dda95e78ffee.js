b75b379f05180d69f0bb84d6c7abb4c3682966b9
Devl197 <ml_aleksndar@hotmail.com>
2020-08-15 17:46:53 +0200

    First commit

---
+++ src/build/precache-manifest.9c6dfaa7941a3f49a9f3dda95e78ffee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32786de1eb027bc68867bf56182ca839",
+    "url": "/index.html"
+  },
+  {
+    "revision": "806cc2758533d692dec4",
+    "url": "/static/css/main.84b4ca96.chunk.css"
+  },
+  {
+    "revision": "b3137f8f841162dc5b2f",
+    "url": "/static/js/2.45200934.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.45200934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806cc2758533d692dec4",
+    "url": "/static/js/main.7ca319ea.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
